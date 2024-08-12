174502b82888ebf5bd8670a22a652408409d35d5
何穎宣 <t110820052@ntut.org.tw>
2024-08-12 23:25:29 +0800

    issue tempfile

--- flask_factory/test/test_upload.py
+++ flask_factory/test/test_upload.py
@@ -3,6 +3,7 @@ import os
 import sys
 import pytest
 from flask import Flask
+from tempfile import NamedTemporaryFile
 
 def test_valid_exe_upload(client):
     with open("testfile.exe", "wb") as f:
@@ -13,9 +14,23 @@ def test_valid_exe_upload(client):
     }
     response = client.post('/upload', data=data, content_type='multipart/form-data')
     assert response.status_code == 200
-    assert response.json == {"message": "File uploaded successfully"}
+    assert response.json == {"message": "File uploaded successfully."}
 
     os.remove("testfile.exe")
+
+    # with NamedTemporaryFile(delete=False, suffix=".exe") as temp_file:
+    #     temp_file.write(b'MZ')
+    #     file_path = temp_file.name
+
+    #     data = {
+    #         'file': (open(file_path, 'rb'), os.path.basename(file_path))
+    #     }
+
+    #     response = client.post('/upload', data=data, content_type='multipart/form-data')
+    #     assert response.status_code == 200
+    #     assert response.json == {"message": "File uploaded successfully."}
+
+    # os.remove(file_path)
 
 def test_no_extension_file_upload(client):
     with open("testfile_no_ext", "wb") as f:
@@ -26,7 +41,7 @@ def test_no_extension_file_upload(client):
     }
     response = client.post('/upload', data=data, content_type='multipart/form-data')
     assert response.status_code == 400
-    assert response.json == {"message": "False type of file"}
+    assert response.json == {"message": "Wrong type of file."}
 
     os.remove("testfile_no_ext")
 
@@ -39,6 +54,6 @@ def test_invalid_file_type_upload(client):
     }
     response = client.post('/upload', data=data, content_type='multipart/form-data')
     assert response.status_code == 400
-    assert response.json == {"message": "False type of file"}
+    assert response.json == {"message": "Wrong type of file."}
 
     os.remove("testfile.txt")
